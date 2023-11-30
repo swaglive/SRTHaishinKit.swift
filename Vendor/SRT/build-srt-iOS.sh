ecab9213b58313e9a7eef1c8d2a55158e0a32ec4
yu840915 <yu840915@gmail.com>
2023-11-30 16:08:51 +0800

    Upgrade libsrt to 1.5.3

--- Vendor/SRT/build-srt-iOS.sh
+++ Vendor/SRT/build-srt-iOS.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+export ver="1.5.3"
 if which $(pwd)/srt >/dev/null; then
   echo ""
 else
-  git clone "git://github.com/Haivision/srt.git"
+  git clone git@github.com:Haivision/srt.git
   pushd srt
-  git checkout refs/tags/v1.4.1
+  git checkout refs/tags/v${ver}
   popd
 fi
 
-export IPHONEOS_DEPLOYMENT_TARGET=9.0
+export IPHONEOS_DEPLOYMENT_TARGET=13.0
 SDKVERSION=$(xcrun --sdk iphoneos --show-sdk-version)
 
 srt() {
@@ -17,19 +18,15 @@ srt() {
 
   mkdir -p ./build/iOS/$3
   pushd ./build/iOS/$3
-  ../../../srt/configure --cmake-prefix-path=$IOS_OPENSSL --ios-platform=$2 --ios-arch=$3 --cmake-toolchain-file=scripts/iOS.cmake --USE_OPENSSL_PC=off
+  ../../../srt/configure --cmake-prefix-path=$IOS_OPENSSL --ios-disable-bitcode=1 --ios-platform=$2 --ios-arch=$3 --cmake-toolchain-file=scripts/iOS.cmake --USE_OPENSSL_PC=off
   make
-  install_name_tool -id "@executable_path/Frameworks/libsrt.1.4.1.dylib" libsrt.1.4.1.dylib
+  install_name_tool -id "@executable_path/Frameworks/libsrt.${ver}.dylib" "libsrt.${ver}.dylib"
   popd
 }
 
 # compile
-srt iphonesimulator SIMULATOR i386
 srt iphonesimulator SIMULATOR64 x86_64
-srt iphoneos OS armv7
-srt iphoneos OS armv7s
 srt iphoneos OS arm64
 
 # lipo
-lipo -output libsrt-iOS.a -create ./build/iOS/x86_64/libsrt.a ./build/iOS/i386/libsrt.a ./build/iOS/arm64/libsrt.a ./build/iOS/armv7/libsrt.a ./build/iOS/armv7s/libsrt.a
-
+lipo -output libsrt-iOS.a -create ./build/iOS/x86_64/libsrt.a  ./build/iOS/arm64/libsrt.a
